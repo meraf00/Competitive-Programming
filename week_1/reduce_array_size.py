2963a7d668f212138b5e0a1b93a8469cef9da854
meraf-0 <lerake0@gmail.com>
2022-01-08 23:02:41 +0300

    Solved

---
+++ week_1/reduce_array_size.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        dict_ = {}
+        for num in arr:
+            if num in dict_.keys():
+                dict_[num] += 1
+            else:
+                dict_[num] = 1
+        
+        values = sorted(dict_.values(), reverse=True)
+        
+        target = len(arr) // 2
+        
+        counter = 0
+        for v in values:
+            if target > 0:            
+                target -= v
+                counter += 1
+                
+        return counter
+            
+        
