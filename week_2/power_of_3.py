969c987a65eab546adf2bf45da60c62810bb4d6e
meraf00 <meraf000@gmail.com>
2022-06-02 10:37:37 +0300

    Solved

--- week_2/power_of_3.py
+++ week_2/power_of_3.py
@@ -3,16 +3,7 @@ class Solution:
         if n == 1:
             return True
         
-        if n % 3 == 0:        
+        if n and n % 3 == 0:        
             return self.isPowerOfThree(n // 3)
         
-        return False
-            
-
-if __name__ == "__main__":
-    s = Solution()
-    a = 27, 9, 27, 0, 1, 54, 0, 999999
-    for i in a:
-        print("I =", i)
-        r = s.isPowerOfThree(i)
-        print(r)
+        return False            
