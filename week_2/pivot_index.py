6c88d68f972d54c9f2a48c36a18fa5c18b50df6d
meraf00 <meraf000@gmail.com>
2022-06-09 10:26:54 +0300

    Solved

---
+++ week_2/pivot_index.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        
+        left = 0      
+        
+        for i, n in enumerate(nums):
+            if left == total - left - n:
+                return i
+            left += n
+        return -1
