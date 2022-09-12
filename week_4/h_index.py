4baa78b873858ba4646d32a1aa257a5c83664fc3
meraf-0 <lerake0@gmail.com>
2022-09-12 22:13:41 +0300

    Solved

---
+++ week_4/h_index.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+        
+        max_h = 0
+        
+        for i, h in enumerate(citations):                 
+            if i + 1 <= h:
+                max_h = i + 1
+                       
+        return max_h
