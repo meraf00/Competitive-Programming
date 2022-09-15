737380a6237fd4cc4682a3ac31c8d77990ce4024
meraf00 <meraf000@gmail.com>
2022-09-15 11:28:15 +0300

    Solved

--- week_4/find_median_from_stream.py
+++ week_4/find_median_from_stream.py
@@ -2,15 +2,18 @@
 https://leetcode.com/problems/find-median-from-data-stream/
 """
 
+import heapq
 from typing import List
 
 class MedianFinder:
 
     def __init__(self):
         self.data = []
+        self.left = []
+        self.right = []
         
 
-    def addNum(self, num: int) -> None:        
+    def addNum_(self, num: int) -> None:        
         if len(self.data) == 0:
             self.data.append(num)
             return
@@ -34,12 +37,27 @@ class MedianFinder:
             self.data.insert(mid-1, num)
         
         
-    def findMedian(self) -> float:        
+    def findMedian_(self) -> float:        
         size = len(self.data)        
         if size % 2 == 0:
             return (self.data[size//2 - 1] + self.data[size//2]) / 2
         return self.data[size//2]
+    
+    
+    # Heap method
+    def addNum(self, num: int) -> None:        
+        heapq.heappush(self.right, num)
+        val = heapq.heappop(self.right)
+        heapq.heappush(self.left, -val)
+        if len(self.right) < len(self.left):
+            val = heapq.heappop(self.left)
+            heapq.heappush(self.right, -val)
         
+        
+    def findMedian(self) -> float:                   
+        if len(self.left) != len(self.right):
+            return self.right[0]
+        return (-self.left[0] + self.right[0]) / 2
 
 
 # Your MedianFinder object will be instantiated and called as such:
