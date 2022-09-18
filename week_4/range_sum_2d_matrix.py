409d024f62964b719d1adbcb93a4d5b855dfafc1
meraf-0 <lerake0@gmail.com>
2022-09-18 14:00:58 +0300

    Solved

---
+++ week_4/range_sum_2d_matrix.py
@@ -0,0 +1,35 @@
+"""
+"""
+
+from typing import List
+
+
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        self.matrix = matrix
+        
+        self.cummulative_sum()
+    
+    def cummulative_sum(self): 
+        """row cummulative"""
+                
+        for r in range(len(self.matrix)):
+            cummulative = 0
+            for c in range(len(self.matrix[0])):
+                self.matrix[r][c] += cummulative
+                cummulative = self.matrix[r][c]        
+    
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        total = 0        
+        for r in range(row1, row2 + 1):
+            if col1 != 0:
+                total += self.matrix[r][col2] - self.matrix[r][col1 - 1]
+            else:
+                total += self.matrix[r][col2]
+                
+        return total
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
