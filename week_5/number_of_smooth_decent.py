50c609a5e064ffaa413110fb6aa4834d86540951
meraf00 <meraf000@gmail.com>
2022-09-21 15:16:48 +0300

    Solved

--- week_5/number_of_smooth_decent.py
+++ week_5/number_of_smooth_decent.py
@@ -8,19 +8,19 @@ class Solution:
     def getDescentPeriods(self, prices: List[int]) -> int:
         if len(prices) <= 1:
             return len(prices)
-        
+
         left = 0
         right = 1
         count = 0
-        
+
         while right < len(prices):
             if prices[right - 1] - prices[right] == 1:
-                right += 1                       
+                right += 1
             else:
                 count += (right - left) * (right - left + 1) // 2
                 left = right
-                right += 1            
-            
+                right += 1
+
             if right == len(prices):
                 count += (right - left) * (right - left + 1) // 2
         return count
