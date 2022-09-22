c2185da97d99895c75ace967c6c64179d7aafc25
meraf00 <meraf000@gmail.com>
2022-09-22 16:34:33 +0300

    Solved

--- week_5/vowels_of_all_substrings.py
+++ week_5/vowels_of_all_substrings.py
@@ -2,7 +2,7 @@
 https://leetcode.com/problems/vowels-of-all-substrings/
 """
 
-class Solution:
+class Solution:    
     def countVowels(self, word: str) -> int:
         vowels = {"a", "e", "i", "o", "u"}        
         
@@ -16,27 +16,4 @@ class Solution:
         for i in range(len(word) // 2):
             sum_ += word[len(word) - i - 1] * (i * (i + 1) + (len(word) - i * 2) * (i + 1))
             
-        return sum_
-
-    # TLE
-    def _countVowels(self, word: str) -> int:
-        vowels = {"a", "e", "i", "o", "u"}        
-        
-        sum_ = 0
-        for i in range(1, len(word) + 1):
-            vowels_in_substring = 0
-            left = right = 0
-            while right < len(word):                                    
-                if word[right] in vowels:
-                    vowels_in_substring += 1
-                
-                if right - left + 1 == i:
-                    sum_ += vowels_in_substring
-                    
-                    if word[left] in vowels:
-                        vowels_in_substring -= 1
-                    left += 1                    
-                    
-                right += 1
-        
         return sum_
