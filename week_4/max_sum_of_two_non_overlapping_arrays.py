750b39f9f0341e9503476afb3d18f4a55e48c394
meraf00 <meraf000@gmail.com>
2022-09-23 16:40:54 +0300

    Rename Variables

--- week_4/max_sum_of_two_non_overlapping_arrays.py
+++ week_4/max_sum_of_two_non_overlapping_arrays.py
@@ -6,7 +6,7 @@ from typing import List
 
 
 class Solution:    
-    def maxSumTwoNoOverlap(self, nums: List[int], firstLen: int, secondLen: int, recurr: bool = True) -> int:
+    def maxSumTwoNoOverlap(self, nums: List[int], rightWindowSize: int, leftWindowSize: int, ignorePosition: bool = True) -> int:
         max_left = 0                
         max_sum = 0
         
@@ -17,7 +17,7 @@ class Solution:
         while rm < len(nums):
             sum_m += nums[rm]
             
-            if rm - lm + 1 > firstLen: # mid window
+            if rm - lm + 1 > rightWindowSize: # mid window
                 sum_m -= nums[lm]
                 lm += 1
             
@@ -27,7 +27,7 @@ class Solution:
                 rl += 1
                 ll += 1                             
                 
-            if lm == secondLen: # start the second window tracking to left of mid window
+            if lm == leftWindowSize: # start the second window tracking to left of mid window
                 while rl < lm:
                     sum_l += nums[rl]  
                     rl += 1                        
@@ -36,7 +36,7 @@ class Solution:
             max_sum = max(max_sum, max_left + sum_m)
             rm += 1
         
-        if recurr:
-            return max(max_sum, self.maxSumTwoNoOverlap(nums, secondLen, firstLen, False))
+        if ignorePosition:
+            return max(max_sum, self.maxSumTwoNoOverlap(nums, leftWindowSize, rightWindowSize, False))
         else:
             return max_sum
