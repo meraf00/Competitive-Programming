bb446ede3863df11d162159946277a7c25bfaa35
meraf00 <meraf000@gmail.com>
2022-09-23 19:27:49 +0300

    Constant time retrival

--- week_1/min_stack.py
+++ week_1/min_stack.py
@@ -1,16 +1,33 @@
+"""
+https://leetcode.com/problems/min-stack/
+"""
+
 class MinStack:
     
     def __init__(self):
-        self.stack = []        
+        self.stack = [] 
+        self.min_stack = []
 
-    def push(self, val: int) -> None:                
+    def push(self, val: int) -> None:   
+        if not self.stack or (self.stack and self.min_stack[-1] >= val):
+            self.min_stack.append(val)
+            
         self.stack.append(val)
 
-    def pop(self) -> None:        
-        del self.stack[-1]
+    def pop(self) -> None:  
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+        return self.stack.pop()
         
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return min(self.stack)
+        return self.min_stack[-1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
