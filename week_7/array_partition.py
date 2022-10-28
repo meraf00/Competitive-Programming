4f529373a82b98d57b07ceb15ca4051453e32cbd
meraf00 <meraf000@gmail.com>
2022-10-28 19:48:33 +0300

    Solution for https://leetcode.com/problems/array-partition

---
+++ week_7/array_partition.py
@@ -0,0 +1,10 @@
+"""https://leetcode.com/problems/array-partition/"""
+
+from typing import List
+
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+
+        return sum(nums[::2])
+
