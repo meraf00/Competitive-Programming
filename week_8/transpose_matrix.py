422ab1034262caf9bdd71e3da4b62b2248e75af9
meraf00 <meraf000@gmail.com>
2022-12-16 19:40:58 +0300

    https://leetcode.com/problems/transpose-matrix/

---
+++ week_8/transpose_matrix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        transposed = []
+
+        n_rows = len(matrix)
+        n_cols = len(matrix[0])
+
+        # create the matrix structure
+        for i in range(n_cols):
+            transposed.append([0] * n_rows)
+
+        for rx in range(n_rows):
+            for cx in range(n_cols):
+                transposed[cx][rx] = matrix[rx][cx]
+
+        return transposed
