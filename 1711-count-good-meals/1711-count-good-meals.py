56635d41dc5bf8091c62064d27b6f514788ddaae
meraf00 <96904152+meraf00@users.noreply.github.com>
2022-12-22 19:04:55 +0300

    Time: 1732 ms (83.54%), Space: 21.9 MB (55.97%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.py
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -2,22 +2,19 @@ class Solution:
     def countPairs(self, deliciousness: List[int]) -> int:
         count = Counter(deliciousness)                
         
-        two_powers = []
-        
-        for i in range(22):
-            two_powers.append(2**i)
+        two_powers = []        
         
         pair_counter = 0
         for d in count.keys():
-            for t in two_powers:                
-                if t - d < d: continue
+            for i in range(22):                
+                if 2**i - d < d: continue
                 
-                if t - d == d:
+                if 2**i - d == d:
                     freq = count[d]
                     pair_counter += freq * (freq - 1) // 2
                     
                 else:
-                    pair_counter += count[d] * count[t-d]
+                    pair_counter += count[d] * count[2**i - d]
                             
         return pair_counter % (10**9 + 7)
                     
