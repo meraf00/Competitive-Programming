e06e2ce0a00e2ff7b11a08500e1bbb420610fa9d
meraf00 <96904152+meraf00@users.noreply.github.com>
2023-02-22 13:06:19 -0800

    Time: 63 ms (18.51%), Space: 15.9 MB (8.09%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        copy = []
+        while head:
+            copy.append(head.val)
+            head = head.next
+                
+        for i in range(0, len(copy), k):
+            if i + k - 1 < len(copy):
+                for j, val in enumerate(reversed(copy[i: i+k])):
+                    copy[i+j] = val
+        
+        head = None
+        current = None
+        for val in copy:
+            if not head:
+                head = ListNode(val)
+                current = head
+                continue
+            current.next = ListNode(val)
+            current = current.next
+        return head
