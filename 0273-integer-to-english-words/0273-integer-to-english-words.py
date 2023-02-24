ee3afb7334bb1bad99efd072cce3cff020e728eb
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2023-02-24 04:52:34 -0800

    Time: 28 ms (93.70%), Space: 13.9 MB (64.43%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -0,0 +1,80 @@
+class Solution:
+    def get_digits(self, num: int) -> int:
+        return len(str(num))        
+    
+    def name3dig(self, num: int, offset: int = 0) -> str:
+        digX = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+        dig1x = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+        dig2x = ["Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety", "Hundred"]
+        
+        postfixes = ["", "Thousand", "Million", "Billion"]
+        
+        postfix = postfixes[offset]
+        
+        digits = self.get_digits(num)
+        
+        output = ""
+        if digits == 1:            
+            output = digX[num]
+            if offset != 0 and num == 0:
+                output = ""
+        
+        elif digits == 2:                        
+            if str(num)[0] == "1":
+                output = "%s" % dig1x[num % 10]
+            
+            elif str(num)[1] == "0":                
+                output = "%s" % dig2x[int(str(num)[0]) - 2]
+            else:
+                output = "%s %s" % (dig2x[int(str(num)[0]) - 2], digX[num % 10])
+        
+        elif digits == 3:            
+            output = "%s Hundred" % digX[int(str(num)[0])]
+             
+            if str(num)[1:] == "00":
+                pass
+            
+            elif str(num)[1] == "0":
+                output += " %s" % digX[num % 10]
+                
+            elif str(num)[1] == "1":
+                output += " %s" % dig1x[num % 10]
+            
+            elif str(num)[2] == "0":                
+                output += " %s" % dig2x[int(str(num)[1]) - 2]
+            else:
+                output += " %s %s" % (dig2x[int(str(num)[1]) - 2], digX[num % 10])
+            
+        if postfix and output:
+            return output + " " + postfix
+        else:
+            return output
+        
+
+    def numberToWords(self, num: int) -> str:
+        grouped = []
+        
+        num = str(num)
+                
+        i = len(num) - 1
+        
+        current = ""
+        while i > -1:            
+            while i > -1 and len(current) < 3:
+                current += num[i]
+                i -= 1
+            grouped.append("".join(list(reversed(current))))
+            current = ""
+        grouped.reverse()                                        
+        
+        grouped = list(map(int, grouped))
+        
+        output = ""
+        for i, g in enumerate(reversed(grouped)):
+            group_name = self.name3dig(g, i)
+            if group_name == "Zero" and i < len(grouped) - 1:
+                continue
+            if group_name:
+                output = group_name + " " + output            
+            
+        return output.strip()
