02a3eb52859ec514a256e9f181ccbfd21fde6de4
meraf00 <meraf000@gmail.com>
2023-02-27 08:27:37 -0800

    Solution for https://codeforces.com/gym/429319/problem/D~

--- contests/camp_contest_1/karen.py
+++ contests/camp_contest_1/karen.py
@@ -1,3 +1,25 @@
 n, k, q = map(int, input().split())
 
-temperatures = []
+recommendation_count = [0] * 200002
+
+for _ in range(n):
+    low, high = map(int, input().split())
+    recommendation_count[low] += 1       
+    recommendation_count[high + 1] += -1
+
+for i in range(1, 200001):
+    recommendation_count[i] += recommendation_count[i - 1]
+
+running_sum = 0
+for i in range(200001):
+    if recommendation_count[i] >= k:
+        running_sum += 1
+    
+    recommendation_count[i] = running_sum
+
+recommendation_count[-1] = 0
+
+for _ in range(q):
+    low, high = map(int, input().split())
+
+    print(recommendation_count[high] - recommendation_count[low-1])
