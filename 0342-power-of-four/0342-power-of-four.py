79236a493cc9c614c612d2e93e0bdd3addefef39
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2023-02-27 23:21:07 -0800

    Time: 29 ms (88.65%), Space: 13.8 MB (93.31%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -3,9 +3,9 @@ class Solution:
         if n == 1:
             return True
         
-        if n <= 0 or n % 4 != 0:
+        elif n < 4:
             return False                
         
-        return self.isPowerOfFour(n // 4) 
+        return self.isPowerOfFour(n / 4) 
         
         
