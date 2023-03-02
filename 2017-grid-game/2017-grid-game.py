ac5b49342e6d4bf2868d1af90cba9994267708e4
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2023-03-02 10:03:02 -0800

    Time: 1210 ms (62.58%), Space: 31.2 MB (13.33%) - LeetHub

--- 2017-grid-game/2017-grid-game.py
+++ 2017-grid-game/2017-grid-game.py
@@ -1,31 +1,27 @@
 class Solution:
-    def gridGame(self, grid: List[List[int]]) -> int:
-        rows = 2
+    def gridGame(self, grid: List[List[int]]) -> int:        
         cols = len(grid[0])
         
+        # suffix sum on row 1
+        # prefix sum on row 2
         for c in range(cols - 1):
-            grid[0][c + 1] += grid[0][c]
+            grid[0][~(c + 1)] += grid[0][~c]
             grid[1][c + 1] += grid[1][c]
         
-        # to avoid if conditions when indexing
-        grid[0].append(0)
-        grid[1].append(0)
-
-        row_1_total = grid[0][-2]        
-        row_2_total = grid[1][-2]        
+        # to avoid if conditions when indexing        
+        grid[0].append(0)                          
+        grid[1].append(0)                          
         
-        min_robot_2_pts = float("inf")
-        
-        turn_index = 0
+        min_robot_2_pts = float("inf")        
         
         for c in range(cols):
-            top_uncollected = row_1_total - grid[0][c]
+            top_uncollected = grid[0][c + 1]
+            
             bottom_uncollected = grid[1][c - 1]
             
             left_for_robot_2 = max(top_uncollected, bottom_uncollected)
             
-            min_robot_2_pts = min(min_robot_2_pts, left_for_robot_2)
-        
+            min_robot_2_pts = min(min_robot_2_pts, left_for_robot_2)        
         
         return min_robot_2_pts
         
