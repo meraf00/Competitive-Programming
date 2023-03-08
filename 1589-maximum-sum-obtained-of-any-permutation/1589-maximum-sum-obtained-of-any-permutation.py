dfeda2a43084c20a50838b931cbb99bd7c9cca80
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2023-03-08 06:51:20 -0800

    Time: 1610 ms (92.57%), Space: 47.8 MB (82.53%) - LeetHub

--- 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
+++ 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
@@ -1,44 +1,31 @@
-class Solution:    
+class Solution:
+    
     def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
-        prefix = [0] * (len(nums) + 1)
+        query_freq = [0] * (len(nums) + 1)
         
         for start, end in requests:
-            prefix[start] += 1
-            prefix[end + 1] -= 1
-                
-        prefix[0] = (-prefix[0], 0)        
-                
-        for i in range(1, len(nums)):
-            val, _ = prefix[i - 1]
-            prefix[i] = (val - prefix[i], i)
-            
-        prefix.pop()
+            query_freq[start] += 1
+            query_freq[end + 1] -= 1
         
-        heapify(prefix)
+        
+        
+        for i in range(1, len(nums)):
+            query_freq[i] += query_freq[i - 1]
+        
+        query_freq.pop()
+        
+        query_freq.sort(reverse=True)
         
         nums.sort(reverse=True)
-                        
-        largest_num_index = 0
         
-        optimal_order = [0] * (len(nums))
-        
-        
-        while prefix:
-            _, index = heappop(prefix)
+                
+        max_sum = 0
+        for num, freq in zip(nums, query_freq):
+            if freq == 0: 
+                break
             
-            optimal_order[index] = nums[largest_num_index]
+            max_sum += freq * num
+
             
-            largest_num_index += 1
-         
-        for i in range(1, len(optimal_order)):
-            optimal_order[i] += optimal_order[i - 1]
-        
-        optimal_order.append(0)
-        
-        max_sum  = 0
-        for start, end in requests:
             
-            max_sum += optimal_order[end] - optimal_order[start-1]
-        
-        
         return max_sum % 1_000_000_007
