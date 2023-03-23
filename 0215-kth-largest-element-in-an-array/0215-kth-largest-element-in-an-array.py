4aa80dba091b06e292977335225ce98bf0bcfebd
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2023-03-23 19:45:03 +0300

    Time: 1048 ms (23.40%), Space: 30.1 MB (26.79%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,13 +1,28 @@
 class Solution:
+    def quicksort(self, start, end, array, k):
+        if start >= end:
+            return
+
+        pivot = array[start]
+
+        write_idx = start + 1
+
+        for read_idx in range(start + 1, end + 1):
+            if array[read_idx] <= pivot:
+                array[read_idx], array[write_idx] = array[write_idx], array[read_idx]
+                write_idx += 1
+
+        array[write_idx - 1], array[start] = array[start], array[write_idx - 1]
+                        
+        if write_idx <= len(array) - k:
+            self.quicksort(write_idx, end, array, k)
+        
+        else:   
+            self.quicksort(start, write_idx - 2, array, k)
+            
+    
+    
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        for i in range(len(nums)):
-            nums[i] *= -1
+        self.quicksort(0, len(nums)-1, nums, k)
         
-        heapify(nums)
-        
-        while k > 1:
-            heapq.heappop(nums)
-            k -= 1
-        
-        return -nums[0]
-        
+        return nums[-k]
