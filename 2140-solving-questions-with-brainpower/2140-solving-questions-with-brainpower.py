a7568581eeea4b7653996f1a481c8efab9690758
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2023-07-11 21:50:00 +0300

    Time: 1846 ms (5.71%), Space: 62.6 MB (56.18%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        cummulative_points = [0] * len(questions)
+        
+        for i in range(len(questions)-1, -1, -1):
+            pts, bp = questions[i]
+            
+            # if question[i] is done
+            if i + bp + 1 < len(questions):
+                cummulative_points[i] = cummulative_points[i + bp + 1]         
+                
+            cummulative_points[i] += pts
+                        
+            if i + 1 < len(questions):
+                cummulative_points[i] = max(cummulative_points[i+1], cummulative_points[i])    
+                    
+        return max(cummulative_points)
+        
+        
