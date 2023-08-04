91fb3b6a731d87bab1fb382306dd9b57dacafe30
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2023-08-04 12:23:19 +0300

    Time: 171 ms (55.38%), Space: 18.5 MB (18.49%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:        
+        # chain lengths ending with word
+        chain_lengths = defaultdict(int)        
+        
+        # sorted with length
+        words.sort(key=lambda word: len(word))
+        
+        for word in words:                            
+            for idx, char in enumerate(word):                 
+                one_char_removed = word[:idx] + word[idx + 1:]                  
+                
+                chain_lengths[word] = max(
+                    chain_lengths[one_char_removed] + 1, 
+                    chain_lengths[word])
+                
+        
+        return max(chain_lengths.values())
+        
+        
