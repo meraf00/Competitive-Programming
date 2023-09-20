6d5bf56e2ef44a41cea2082789dd3babf222c107
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2023-09-20 23:59:25 +0300

    Time: 47 ms (90.75%), Space: 17.7 MB (87.83%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        odds = set('13579')
+                
+        
+        for i in range(len(num) - 1, -1, -1):            
+            if num[i] in odds:
+                return num[:i+1]
+        
+        return ''
+    
