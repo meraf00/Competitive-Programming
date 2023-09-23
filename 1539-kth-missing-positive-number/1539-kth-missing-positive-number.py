29df050fbca59695a6cf604b60cdb402ea79159e
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2023-09-23 19:20:42 +0300

    Time: 63 ms (35.21%), Space: 16.4 MB (54.26%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -1,5 +1,32 @@
-class Solution:    
-    def findKthPositive(self, arr: List[int], k: int) -> int:          
+class Solution:
+    def countMissing(self, nums, idx):
+        expected = nums[idx]
+        return expected - (idx + 1)
+    
+    def findKthPositive(self, nums: List[int], k: int) -> int:          
+        low = 0
+        high = len(nums) - 1    
+
+        while low < high:
+            mid = (low + high) // 2
+
+            if self.countMissing(nums, mid) < k:
+                low = mid + 1                        
+
+            else:
+                high = mid - 1
+
+        n_missing = self.countMissing(nums, high)                   
+        
+        if high < 0:
+            return k
+                
+        if n_missing >= k:             
+            return nums[high] + k - n_missing - 1
+        
+        return nums[high] + k - n_missing
+    
+    def findKthPositive_(self, arr: List[int], k: int) -> int:          
         number_of_missing_num = arr[0] - 1
         
         for i in range(1, len(arr)):
