982af8fbf529367bb522b3e41f88ee083f4a3186
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2023-09-29 09:53:40 +0300

    Time: 40 ms (60.08%), Space: 16.2 MB (90.13%) - LeetHub

--- 0677-map-sum-pairs/0677-map-sum-pairs.py
+++ 0677-map-sum-pairs/0677-map-sum-pairs.py
@@ -1,18 +1,51 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+        self.value = 0
+        
+
 class MapSum:
 
     def __init__(self):
-        self.map_sum = defaultdict(int)
-        self.key_value = defaultdict(int)
+        self.root = TrieNode()
 
-    def insert(self, key: str, val: int) -> None:        
-        for i in range(1, len(key) + 1):
-            self.map_sum[key[:i]] += val - self.key_value[key]
-                
-        self.key_value[key] = val
+    def insert(self, key: str, val: int) -> None:
+        current = self.root
         
-
-    def sum(self, prefix: str) -> int:        
-        return self.map_sum[prefix]
+        for char in key:
+            if char not in current.children:
+                current.children[char] = TrieNode()
+            
+            current = current.children[char]
+        
+        current.value = val
+        
+        current.is_end = True
+                        
+    def sum(self, prefix: str) -> int:
+        current = self.root
+        
+        for char in prefix:
+            if char not in current.children:
+                return 0
+        
+            current = current.children[char]
+        
+        stack = [current]
+        
+        value_sum = 0
+        
+        while stack:
+            node = stack.pop()
+            
+            if node.is_end:
+                value_sum += node.value
+                                        
+            for child in node.children.values():                
+                stack.append(child)
+                
+        return value_sum
         
 
 
