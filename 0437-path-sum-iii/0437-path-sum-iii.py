0d5ff6b90e1da6454c7ed28404b69d2a9818133b
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2023-10-06 11:41:24 +0300

    Time: 56 ms (69.98%), Space: 17.9 MB (33.25%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -5,33 +5,33 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
-    def count_path(self, node, counter, running_sum, target_sum):
-        if not node:
-            return
-        
-        running_sum += node.val                
-        
-        self.total_path += counter[running_sum - target_sum]
-        
-        counter[running_sum] += 1
-        
-        self.count_path(node.left, counter, running_sum, target_sum)                  
-        
-        self.count_path(node.right, counter, running_sum, target_sum)
-        
-        counter[running_sum] -= 1
-        
-                
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        running_sum = 0
         
         counter = defaultdict(int)
+        counter[0] = 1   
         
-        counter[0] = 1
-                                                       
-        self.total_path = 0
+        count = 0
         
-        self.count_path(root, counter, running_sum, targetSum)
+        def dfs(root, running_sum):
+            nonlocal count
+            
+            if not root:                
+                return
+            
+            running_sum += root.val                        
+                        
+            count += counter[running_sum - targetSum]
+            
+            counter[running_sum] += 1
+            
+            dfs(root.left, running_sum)
+            
+            dfs(root.right, running_sum)
+            
+            counter[running_sum] -= 1
+                        
         
-        return self.total_path
+        dfs(root, 0)
+    
+        return count
+        
