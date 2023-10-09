3a178623ae64adbe9905ebfa400f2fcac6fd33d1
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2023-10-09 15:28:39 +0300

    Time: 45 ms (13.33%), Space: 16.3 MB (52.35%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,66 +1,48 @@
 class Solution:    
     def strStr(self, haystack: str, needle: str) -> int:        
-        MOD = 10 ** 9 + 7
-        
         len_n = len(needle)
+        
+        lps = [0] * len_n
+                
+        i, j = 0, 1
+        
+        while j < len_n and i < len_n:
+            if needle[i] == needle[j]:
+                lps[j] = i + 1
+                i += 1
+                j += 1
+            
+            else:
+                if i == 0:
+                    j += 1
+                
+                else:
+                    i = lps[i - 1]
+        
+        i = 0
+        j = 0
+        
         len_h = len(haystack)
         
-        if len_n > len_h:
-            return -1
-        
-        base = 27
-        
-        power = {0: 1}
-        
-        for i in range(1, len_n + 1):
-            power[i] = power[i - 1] * base
-        
-        
-        def poll_first(h, char):
-            asci = ord(char) - ord('a') + 1              
-            return (h - asci * power[len_n - 1]) % MOD
-        
-        def add_last(h, char):
-            return (h * base + (ord(char) - ord('a') + 1)) % MOD
-        
-        def compute_hash(string):
-            n = len(string)
-            hash = 0
-            
-            for i, char in enumerate(string):
-                asci = ord(char) - ord('a') + 1
+        while j < len_h and i < len_n:
+            if needle[i] == haystack[j]:
+                i += 1
+                j += 1
                 
-                hash += asci * power[n - i - 1]
+                if i >= len_n:
+                    return j - i
+            
+            else:
+                if i == 0:
+                    j += 1
                 
-            return hash % MOD                
+                else:
+                    i = lps[i - 1]
         
-        def compare_substring(left, right):
-            for i in range(left, right + 1):                
-                if haystack[i] != needle[i - left]:
-                    return False
-            
-            return True
-                                        
-        target = compute_hash(needle)                 
-        
-        window_hash = compute_hash(haystack[:len_n])
-        
-        if target == window_hash:
-            return 0
-                                
-        left = 0      
-                                
-        for right in range(len_n, len_h):
-            window_hash = poll_first(window_hash, haystack[left]) 
-                                        
-            left += 1                                                  
-                        
-            window_hash = add_last(window_hash, haystack[right]) 
+        return -1
+                    
             
             
-            if window_hash == target:
-                if compare_substring(left, right):
-                    return left
-                                                                 
-        return -1 
+        
+        
         
