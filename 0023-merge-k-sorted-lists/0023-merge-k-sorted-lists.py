ba07c9921eca404014868f721aa360b45ddf79f8
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-03-21 17:32:17 +0300

    Time: 105 ms (23.59%), Space: 19.4 MB (71.96%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,35 +3,103 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def merge2Lists(self, l1, l2):
-        dummy = ListNode() 
-        
-        current = dummy
-        while l1 and l2:
-            if l1.val <= l2.val:
-                current.next = l1
-                l1 = l1.next
-            else:
-                current.next = l2
-                l2 = l2.next
-            
-            current = current.next
-        
-        if l1:
-            current.next = l1
-        if l2:
-            current.next = l2
-        
-        return dummy.next    
-                
+class Solution:    
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
             return
         
-        merged = lists[0]
-        for lst in lists[1:]:
-            merged = self.merge2Lists(merged, lst) 
+        def parent(j):
+            return (j - 1) // 2
+
+        def swap(heap,i, j):
+            heap[i], heap[j] = heap[j], heap[i] 
             
-        return merged
+
+        def heapify(arr):
+            n = len(arr)
             
+            for i in range(n // 2 - 1, -1, -1):
+                heapify_down(arr, n, i)
+        
+        def heappush(heap, value):
+            heap.append(value)
+            currentIdx = len(heap) - 1
+            heapify_up(heap, currentIdx)
+        
+        def heappop(heap):
+            if not heap:
+                return None
+
+            min_value = heap[0]
+            heap[0] = heap[-1]
+            heap.pop()
+
+            current = 0
+            heapify_down(heap, len(heap), current) 
+
+            return min_value
+        
+        def heapify_up(heap, j):
+            p = parent(j)
+            if j > 0 and heap[j].val < heap[p].val:
+                swap(heap, j, p) 
+                heapify_up(heap, p)        
+        
+        def heapify_down(heap, n, current_idx):
+            small_child_idx = current_idx
+            left_idx = 2 * current_idx + 1
+            right_idx = 2 * current_idx + 2
+
+            if left_idx < n and heap[left_idx].val < heap[small_child_idx].val:
+                small_child_idx = left_idx 
+
+            if right_idx < n and heap[right_idx].val < heap[small_child_idx].val:
+                small_child_idx = right_idx
+
+            if small_child_idx != current_idx:
+                swap(heap, current_idx, small_child_idx)
+                heapify_down(heap, n, small_child_idx)
+                
+        write = len(lists) - 1
+        
+        while lists[write] == None and write > -1:
+            write -= 1
+        
+        if write == -1:
+            return
+        
+        for i in range(len(lists)): 
+            if i >= write:
+                break
+                
+            if lists[i] == None:
+                swap(lists, i, write)
+                write -= 1                        
+        
+        
+        while lists and not lists[-1]:
+            lists.pop()
+            
+        
+        
+        dummy = ListNode()
+        
+        heapify(lists)
+        
+        current = dummy
+        
+        while lists:
+            min_node = heappop(lists)
+            
+            if min_node:            
+                current.next = min_node
+
+                if min_node.next:            
+                    heappush(lists, min_node.next)
+
+                min_node.next = None
+                current = current.next
+        
+        return dummy.next
+            
+        
