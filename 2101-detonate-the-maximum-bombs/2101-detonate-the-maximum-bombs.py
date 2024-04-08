adb56774babc0d8c4b43c844169a8457d1735448
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-04-08 13:22:34 +0300

    Time: 5057 ms (5.03%), Space: 17 MB (63.55%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -1,51 +1,45 @@
 class Solution:
-    def get_neighbours(self, bomb_idx, bombs):
-        x0, y0, r0 = bombs[bomb_idx]                        
-
-        for bomb2_idx in range(len(bombs)):
-            if bomb_idx == bomb2_idx:
-                continue
-
-            x, y, r = bombs[bomb2_idx]
-
-            if (x0 - x) ** 2 + (y0 - y) ** 2 <= r0 ** 2:
-                yield bomb2_idx
-                
-                
-    def maximumDetonation(self, bombs: List[List[int]]) -> int:                                            
-        bombs = list(map(tuple, bombs))
-        n_bombs = len(bombs)
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        n = len(bombs)
         
         graph = defaultdict(list)
         
-        for idx in range(n_bombs):
-            graph[idx].extend(self.get_neighbours(idx, bombs))            
-
-        
-        max_count = 0
-        
-        for bomb_idx in range(n_bombs):
-                
-            stack = [bomb_idx]
+        for i, bomb in enumerate(bombs):
+            bomb[-1] *= bomb[-1]
+                        
+        for i in range(n):
+            bomb_ix, bomb_iy, bomb_ir = bombs[i]
             
-            count = 0
+            for j in range(i + 1, n):        
+                bomb_jx, bomb_jy, bomb_jr = bombs[j]
+                
+                gap = (bomb_ix - bomb_jx) ** 2 + (bomb_iy - bomb_jy) ** 2
+                
+                if gap <= bomb_ir:
+                    graph[i].append(j)
+                
+                if gap <= bomb_jr:
+                    graph[j].append(i)
+        
+        max_detonated = 0
+        
+        for i in range(n):
+            stack = [i]
             
             visited = set()
             
-            while stack:                
-                current  = stack.pop()
+            while stack:
+                node = stack.pop()
                 
-                if current in visited:
-                    continue
-                                
-                visited.add(current) 
+                visited.add(node)
                 
-                count += 1                                
-                
-                for nbr in graph[current]:
-                    if nbr not in visited:                        
+                for nbr in graph[node]:
+                    if nbr not in visited:
                         stack.append(nbr)
             
-            max_count = max(count, max_count)
-                                
-        return max_count
+            max_detonated = max(max_detonated, len(visited))
+        
+        return max_detonated
+        
+        
+        
