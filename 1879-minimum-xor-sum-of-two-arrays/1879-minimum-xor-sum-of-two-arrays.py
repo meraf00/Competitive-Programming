dc36e29fa24632a45cafcdc2e746c374a48c2304
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-05-17 18:47:51 +0300

    Time: 2981 ms (5.26%), Space: 171.6 MB (5.26%) - LeetHub

---
+++ 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minimumXORSum(self, nums1: List[int], nums2: List[int]) -> int:
+        length = len(nums1)
+        
+        @cache
+        def dp(bitmask, k):
+            if k >= length:                
+                if bitmask + 1 == (1 << (length)):
+                    return 0
+                return float('inf')
+            
+            result = float('inf')
+            
+            for i, n in enumerate(nums2):
+                if bitmask & (1 << i):
+                    continue
+                
+                result = min(
+                    dp(bitmask ^ (1 << i), k + 1) + (n ^ nums1[k]), 
+                    dp(bitmask, k + 1),
+                    result)
+                
+            
+            return result
+                
+                
+        return dp(0, 0)    
+        
+                
